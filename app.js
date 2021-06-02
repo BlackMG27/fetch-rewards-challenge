68953059f58a18a6af5c4754943b92ffe16ed4ea
BlackMG27 <oike279@gmail.com>
2021-06-02 07:59:43 -0400

    filtered, grouped, and sorted the items

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //setup the variables 
 const itemArr = [];
+let thisList,
+    newList;
 
 fetch(`https://fetch-hiring.s3.amazonaws.com/hiring.json`)
     .then(res => res.json())
@@ -10,6 +12,31 @@ fetch(`https://fetch-hiring.s3.amazonaws.com/hiring.json`)
             return notA;
         })
         console.log(data)
+        //group the list by id 
+        thisList = data.reduce((a, d) => {
+            //if the list doesn't exist in the array
+            if(!a[d.listId]){
+                //sets the listId as an array 
+                a[d.listId] = [d]
+                //also checks to see if that object is in the array
+            }else{
+                //pushes the object into that specific array
+                a[d.listId].push(d)
+            }
+            return a;
+        }, {});
+        console.log(thisList);
+        //gets the values of object and turns them into an array
+        newList = Object.values(thisList);
+        console.log(newList)
+
+        //sort the objects in the array
+        for(let list in newList){
+            newList[list].sort((a,b) => {
+                return a.id - b.id
+            })
+        }
+        console.log(newList)
     })
 
     
